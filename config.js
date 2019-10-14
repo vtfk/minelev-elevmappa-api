a7062b2107918e09ff1c118c15277d97fdbdc915
maccyber <jonas.enge@gmail.com>
2019-10-14 12:50:01 +0200

    Make code easier to read

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
 module.exports = {
   username: process.env.P360_WS_USERNAME || 'domain/username',
   password: process.env.P360_WS_PASSWORD || 'password',
-  baseUrl: process.env.P360_WS_BASE_URL || 'http://p360server.domain.no:8088/SI.WS.Core/SIF/'
+  baseUrl: process.env.P360_WS_BASE_URL || 'http://p360server.domain.no:8088/SI.WS.Core/SIF/',
+  auth: {
+    /*
+     * Find your Tenant ID for Azure in https://login.windows.net/<your-tenant-name>/.well-known/openid-configuration
+     * Example: 08f3813c-0f00-000f-9aec-16ef7cbf477a
+    */
+    tenantId: process.env.MOA_TENANT_ID || 'tenant-id',
+    /*
+     * Your App ID for the registered app at https://portal.azure.com -> Azure
+     * Active Directory -> App registrations
+    */
+    appId: process.env.MOA_APP_ID || 'your-app-id'
+  }
 }
